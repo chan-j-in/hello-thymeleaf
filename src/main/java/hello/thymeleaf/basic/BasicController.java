86a2c26956b757c277a5fd75b0613fba038c9b69
l__o_ <genius8111@naver.com>
2025-01-02 14:10:50 +0900

    feat : literal

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -74,11 +74,17 @@ public class BasicController {
         return "basic/data";
     }
 
-    @GetMapping("link")
+    @GetMapping("/link")
     public String link(Model model) {
         model.addAttribute("param1", "data1");
         model.addAttribute("param2", "data2");
         return "basic/link";
+    }
+
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
     }
 
     @Data
